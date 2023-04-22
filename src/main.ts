55e89410bb639ba497a2c7b40da9c1fbca70a0d2
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2023-04-22 04:01:46 -0400

    matrix math prototype done

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ interface Array<T> {
   compact(): Array<T>;
   deepFlat(): Array<T>;
   math(operand: string, number?: number): Array<T>;
+  matrixMath(callback: (n: number, i: number) => number, arr2: any[]): Array<T>;
 };
 
 const alphabet = new Set('abcdefghijklmnopqrstuvwxyz');
 const numbers = new Set('0123456789');
 
-let testArray = [1, 2, 3, 4, , 'abc', 'hi', 10];
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let testArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // shuffles array in place (mutates array)
 Array.prototype.shuffle = function() {
@@ -38,7 +40,7 @@ Array.prototype.deepFlat = function() {
     return this.toString().split(',').map((n) => isNaN(Number(n)) ? n : Number(n));
 }
 
-// perform a type of math on each index of an array (mutates array)
+// perform an operation on each index (mutates array)
 Array.prototype.math = function(operand: string, number: number) {
     return this.map((n) => {
         if (typeof n === 'number') {
@@ -66,9 +68,18 @@ Array.prototype.math = function(operand: string, number: number) {
     });
  }
     
+ // perform an operation between two indeces of two arrays (creates new array)
+ Array.prototype.matrixMath = function(callback, arr2) {
+    return this.map((n, i) => {
+        if (typeof n === 'number') {
+            return callback(n, arr2[i]);
+        } else return n;
+    });
+ }
+    
 
 
 
-let x = testArray.math("%", 2);
+let x = testArray.matrixMath(((n, n2) => n + n2), testArray2);
 x;
 testArray;
